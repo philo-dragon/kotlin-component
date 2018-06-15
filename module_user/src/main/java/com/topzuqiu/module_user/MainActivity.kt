2b6bddabab9fde447953a9cf442baa20305d5037
Philo <panfeilong2012@163.com>
2018-06-15 16:27:16 +0800

    kotlin arouter 配置

--- module_user/src/main/java/com/topzuqiu/module_user/MainActivity.kt
+++ module_user/src/main/java/com/topzuqiu/module_user/MainActivity.kt
@@ -1,8 +1,7 @@
 package com.topzuqiu.module_user
 
-import android.databinding.DataBindingUtil
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import com.alibaba.android.arouter.facade.annotation.Route
 
 @Route(path = "/module_user/activity")
@@ -10,6 +9,6 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_test)
     }
 }
