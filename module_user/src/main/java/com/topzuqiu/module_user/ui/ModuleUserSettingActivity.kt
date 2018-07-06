0b605407c7689cae0121a2df35c62244e355c126
philo <panfeilong2012@163.com>
2018-07-06 23:34:54 +0800

    解决kotlin与databinding冲突

--- module_user/src/main/java/com/topzuqiu/module_user/ui/ModuleUserSettingActivity.kt
+++ module_user/src/main/java/com/topzuqiu/module_user/ui/ModuleUserSettingActivity.kt
@@ -1,16 +1,29 @@
 package com.topzuqiu.module_user.ui
 
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
 import com.alibaba.android.arouter.facade.annotation.Route
+import com.topzuqiu.lib_common.base.BaseActivity
+import com.topzuqiu.lib_common.di.AppComponent
 import com.topzuqiu.lib_common.utils.RouteUtil
 import com.topzuqiu.module_user.R
+import com.topzuqiu.module_user.databinding.ModuleUserActivitySettingBinding
 
 @Route(path = RouteUtil.Companion.MODULE_USER_ACTIVITY_SETTING)
-class ModuleUserSettingActivity : AppCompatActivity() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.module_user_activity_setting)
+class ModuleUserSettingActivity : BaseActivity<ModuleUserActivitySettingBinding>() {
+    override fun getContentView(): Int {
+        return R.layout.module_user_activity_setting
     }
+
+    override fun componentInject(appComponent: AppComponent?) {
+    }
+
+    override fun initView() {
+    }
+
+    override fun setToolBar() {
+    }
+
+    override fun initData() {
+    }
+
 }
